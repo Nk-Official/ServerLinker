f00835fd28e335e2706686674f71715986aa42aa
Nk-Official-Softprodigy <namrata_khanduri@softprodigy.com>
2021-03-12 15:51:00 +0530

    Initial Commit

---
+++ ServerConnect/ServerConnect.h
@@ -0,0 +1,19 @@
+//
+//  ServerConnect.h
+//  ServerConnect
+//
+//  Created by user on 12/03/21.
+//  Copyright © 2021 Namrata Khanduri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ServerConnect.
+FOUNDATION_EXPORT double ServerConnectVersionNumber;
+
+//! Project version string for ServerConnect.
+FOUNDATION_EXPORT const unsigned char ServerConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServerConnect/PublicHeader.h>
+
+
